b406091a234e7b9e7e37f9c465116e1587fc8d6c
s25662-pj <92219358+s25662-pj@users.noreply.github.com>
2022-03-19 12:54:01 +0100

    Update Main.java
    Teraz dopiero bo upsi zrobilem

--- kolokwium2/Main.java
+++ kolokwium2/Main.java
@@ -3,15 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        sum(new String[]{"1", "3", "2", "1"});
+        System.out.println(sum(new String[]{"1", "3", "2", "1"}));
     }
 
 
-    public static void sum(String[] arr) {
+    public static String sum(String[] arr) {
         var wynik = 0;
         for(var i = 0; i < arr.length; i++){
             wynik += Integer.parseInt(arr[i]);
         }
-        System.out.println(String.valueOf(wynik));
+        return String.valueOf(wynik);
     }
 }
